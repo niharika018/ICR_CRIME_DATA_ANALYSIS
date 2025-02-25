d6e04fd320d5fffbc07551418a7075f3faa25107
dalo9024 <dalo9024@colorado.edu>
2025-02-24 21:15:42 -0700

    Update DENhexgrids.py

--- CREATE_GRID/DENhexgrids.py
+++ CREATE_GRID/DENhexgrids.py
@@ -4,11 +4,16 @@ import geopandas as gpd
 import geohexgrid as ghg
 import matplotlib.pyplot as plt
 
+savefilename = 'grid.geojson' #.geojson
 r = 0.005
 den = gpd.read_file("DENVER.geojson")
 grid = ghg.make_grid_from_gdf(den, R=r)
 
-base = den.plot(color="black", figsize=(10, 10), aspect="equal", alpha = 0.5)
-grid.plot(ax=base, color="white", edgecolor="red", alpha=0.5)
+# =============================================================================
+#test plotting
+#base = den.plot(color="black", figsize=(10, 10), aspect="equal", alpha = 0.5)
+#grid.plot(ax=base, color="white", edgecolor="red", alpha=0.5)
+#plt.show()
+# =============================================================================
 
-plt.show()
+grid.to_file(savefilename, driver='GeoJSON') 
